72a5ec3151d9d074539f750f468a97ef1e726e74
Anthony Chu <antchu@microsoft.com>
2020-05-16 00:36:24 -0700

    Small update

--- server.ts
+++ server.ts
@@ -3,7 +3,7 @@ import { Application, Context } from "https://deno.land/x/oak@v4.0.0/mod.ts";
 const app = new Application();
 
 app.use((ctx: any) => {
-  ctx.response.body = "Hello from Deno on Azure! 🦕";
+  ctx.response.body = "Hello from Deno on Azure!!! 🦕";
   console.log(`request received: ${ctx.request.url}`);
 });
 
